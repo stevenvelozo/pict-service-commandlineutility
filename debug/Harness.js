fea1b2e460c8912e9ec8ab8f73cc94c5ab0d6c19
Steven Velozo <steven@velozo.com>
2025-11-02 06:49:40 -0800

    New version of commander

--- debug/Harness.js
+++ debug/Harness.js
@@ -1,5 +1,5 @@
 const libDebug = require('./DebugProgram.js');
-libDebug.run(['node', 'Harness.js', 'kobayashi']);
+//libDebug.run(['node', 'Harness.js', 'kobayashi']);
 
 //const libDebug = require('../example_program/Pict-CLI-Example-Program.js');
 //libDebug.run(['node', 'Harness.js', 'name']);
@@ -12,5 +12,5 @@ libDebug.run(['node', 'Harness.js', 'kobayashi']);
 // These work with any standard utility
 
 //libDebug.run(['node', 'Harness.js']);
-//libDebug.run(['node', 'Harness.js', 'explain-config']);
+libDebug.run(['node', 'Harness.js', 'explain-config']);
 //libDebug.run(['node', 'Harness.js', '-v']);
--- source/Pict-CLIProgram-CommandLineCommand-ExplainConfig.js
+++ source/Pict-CLIProgram-CommandLineCommand-ExplainConfig.js
@@ -16,7 +16,24 @@ class ExplainConfigCommand extends libCommandLineCommand
 		this.log.info(`Gathering and explaining configuration...`);
 		// By passing in false, it doesn't apply the config and just explains it and returns the explanation object.
 		let tmpConfigurationExplanation = this.fable.gatherProgramConfiguration(false);
-		this.log.info(`Explanation`, tmpConfigurationExplanation);
+		// Generatee an explanation string
+		let tmpExplanationString = JSON.stringify(tmpConfigurationExplanation, null, 4);
+
+		const tmpExplanationLines = tmpExplanationString.split('\n');
+		for (let i = 0; i < tmpExplanationLines.length; i++)
+		{
+			if (tmpExplanationLines[i].toLowerCase().includes('"password":'))
+			{
+				const tmpParts = tmpExplanationLines[i].split(':');
+				if (tmpParts.length >= 2)
+				{
+					tmpExplanationLines[i] = `${tmpParts[0]}: "***REDACTED***"`;
+				}
+			}
+		}
+		tmpExplanationString = tmpExplanationLines.join('\n');
+
+		console.log(tmpExplanationString);
 	};
 }
 
